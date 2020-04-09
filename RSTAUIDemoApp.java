e9a670932b9df6bec33eb658f1e74f2add9bb9f0
Nimish Bongale <nimishnb98@gmail.com>
2020-04-10 01:04:51 +0530

    Tree Added

--- RSTAUIDemoApp.java
+++ RSTAUIDemoApp.java
@@ -21,6 +21,13 @@ import org.fife.ui.rsyntaxtextarea.*;
 import java.io.*;
 import java.util.*;
 
+import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.*;
+
+import java.nio.file.*;
+
 
 public final class RSTAUIDemoApp extends JFrame implements SearchListener {
 	private CollapsibleSectionPanel csp;
@@ -39,11 +46,13 @@ public final class RSTAUIDemoApp extends JFrame implements SearchListener {
 		JPanel contentPane = new JPanel(new BorderLayout());
 		setContentPane(contentPane);
 		csp = new CollapsibleSectionPanel();
-		contentPane.add(csp);
-
+		JPanel jp=new JPanel();
+		jp.add(new FileTree(new File(".")));
+		jp.add(csp);
 		setJMenuBar(createMenuBar());
+		contentPane.add(jp);
 
-		textArea = new RSyntaxTextArea(25, 80);
+		textArea = new RSyntaxTextArea(40, 150);
 		textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
 		textArea.setCodeFoldingEnabled(true);
 		textArea.setMarkOccurrences(true);
@@ -379,9 +388,86 @@ public final class RSTAUIDemoApp extends JFrame implements SearchListener {
 		});
 	}
 
-    /**
-     * Opens the "Go to Line" dialog.
-     */
+	 class FileTree extends JPanel {
+  /** Construct a FileTree */
+  public FileTree(File dir) {
+    setLayout(new BorderLayout());
+
+    // Make a tree list with all the nodes, and make it a JTree
+    JTree tree = new JTree(addNodes(null, dir));
+
+    // Add a listener
+    tree.addTreeSelectionListener(new TreeSelectionListener() {
+      public void valueChanged(TreeSelectionEvent e) {
+		// System.out.println(tree.getLastSelectedPathComponent().getUserObject().toString());
+		// File f = new File(node.toString());  
+        //     String abso = f.getPath(); 
+		String selectedPath = tree.getSelectionPath().toString();
+		String ans[]=selectedPath.split(",", 0);
+		String pathy=ans[ans.length-2].toString()+"\\"+(ans[ans.length-1].toString().substring(0,ans[ans.length-1].length()-1)).trim();
+		 try {
+					setTitle("N45Editor"+" | "+pathy);
+					textArea.setText(" ");
+                    Scanner scan = new Scanner(new FileReader(pathy.substring(1,pathy.length())));
+                    while (scan.hasNext()) {
+						textArea.setText(textArea.getText()+"\n"+scan.nextLine());
+                    }
+                } catch (Exception ex) { // catch any exceptions, and...
+                    // ...write to the debug console
+                    System.err.println(ex.getMessage());
+                }
+
+     }
+    });
+
+    // Lastly, put the JTree into a JScrollPane.
+    JScrollPane scrollpane = new JScrollPane();
+    scrollpane.getViewport().add(tree);
+	// scrollpane.getViewport().setPreferredSize(new Dimension(10,200));
+    add(BorderLayout.CENTER, scrollpane);
+  }
+
+  /** Add nodes from under "dir" into curTop. Highly recursive. */
+  DefaultMutableTreeNode addNodes(DefaultMutableTreeNode curTop, File dir) {
+    String curPath = dir.getPath();
+    DefaultMutableTreeNode curDir = new DefaultMutableTreeNode(curPath);
+    if (curTop != null) { // should only be null at root
+      curTop.add(curDir);
+    }
+    Vector ol = new Vector();
+    String[] tmp = dir.list();
+    for (int i = 0; i < tmp.length; i++)
+      ol.addElement(tmp[i]);
+    Collections.sort(ol, String.CASE_INSENSITIVE_ORDER);
+    File f;
+    Vector files = new Vector();
+    // Make two passes, one for Dirs and one for Files. This is #1.
+    for (int i = 0; i < ol.size(); i++) {
+      String thisObject = (String) ol.elementAt(i);
+      String newPath;
+      if (curPath.equals("."))
+        newPath = thisObject;
+      else
+        newPath = curPath + File.separator + thisObject;
+      if ((f = new File(newPath)).isDirectory())
+        addNodes(curDir, f);
+      else
+        files.addElement(thisObject);
+    }
+    // Pass two: for files.
+    for (int fnum = 0; fnum < files.size(); fnum++)
+      curDir.add(new DefaultMutableTreeNode(files.elementAt(fnum)));
+    return curDir;
+  }
+
+  public Dimension getMinimumSize() {
+    return new Dimension(200, 400);
+  }
+
+  public Dimension getPreferredSize() {
+    return new Dimension(200, 643);
+  }
+	 }
 	private class GoToLineAction extends AbstractAction {
 
 		GoToLineAction() {

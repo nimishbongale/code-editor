3a7616c4ab52ac2f500d2f9acf6b945d177d0485
Nimish Bongale <nimishnb98@gmail.com>
2020-04-16 14:39:57 +0530

    Changes

--- RSTAUIDemoApp.java
+++ RSTAUIDemoApp.java
@@ -604,7 +604,7 @@ public final class RSTAUIDemoApp extends JFrame implements SearchListener {
 	public static void main(String args[]) {
 		SwingUtilities.invokeLater(() -> {
 			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
